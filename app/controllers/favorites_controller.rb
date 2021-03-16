d79b686a99f33b34fc0494b2f99232afaee1a5fd
Ubuntu <ubuntu@ip-172-31-16-24.ap-northeast-1.compute.internal>
2021-03-16 01:31:12 +0000

    likes

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,13 +5,13 @@ class FavoritesController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.favorite(micropost)
     flash[:success]="お気に入り登録しました。"
-    redirect_to user
+    redirect_to user_path(id: current_user)
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.unfavorite(micropost)
     flash[:success]="お気に入り解除じました。"
-    redirect_to user
+    redirect_to user_path(id: current_user)
   end
 end
